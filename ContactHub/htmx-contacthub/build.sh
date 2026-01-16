3d920e58caaaae977bdb3ed45f53c021219ddbe4
fron-tn <frontn365@gmail.com>
2026-01-16 05:11:24 +0200

    Update build.sh

--- ContactHub/htmx-contacthub/build.sh
+++ ContactHub/htmx-contacthub/build.sh
@@ -11,4 +11,13 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py createsuperuser --noinput || true
+# Create superuser from environment variables
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='${SUPERUSER_USERNAME}').exists():
+    User.objects.create_superuser('${SUPERUSER_USERNAME}', '${SUPERUSER_EMAIL}', '${SUPERUSER_PASSWORD}')
+    print('Superuser created successfully')
+else:
+    print('Superuser already exists')
+END
